9e23f5166b746234190c4e2b5512a1fc550b7e7b
Sergi Alcaraz <sergi_alca@hotmail.com>
2020-06-02 20:58:26 +0200

    Updates

--- .cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-plugin-anchor-links/gatsby-browser.js'),
-      options: {"plugins":[],"offset":-50},
-    },{
-      plugin: require('../gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
